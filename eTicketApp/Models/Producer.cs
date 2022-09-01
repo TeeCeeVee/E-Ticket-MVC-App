987790950fa0663dbdb0f88a10976876f8b1b366
thea Villanueva <tei.villanueva@gmail.com>
2022-08-31 20:46:09 -0700

    Add Producers View Page

--- eTicketApp/Models/Producer.cs
+++ eTicketApp/Models/Producer.cs
@@ -1,10 +1,20 @@
-﻿namespace eTicketApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicketApp.Models
 {
     public class Producer
     {
+        [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Profile Picture")]
+
         public string ProfilePictureUrl { get; set; }
+
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
 
         //Relationships
