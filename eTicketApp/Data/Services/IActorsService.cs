32059fb593e3729c8c1a99eabdd23acbce63d333
thea Villanueva <tei.villanueva@gmail.com>
2022-09-02 19:40:03 -0700

    Add Actors Service

---
+++ eTicketApp/Data/Services/IActorsService.cs
@@ -0,0 +1,27 @@
+﻿using eTicketApp.Models;
+
+namespace eTicketApp.Data.Services
+{
+    //define methods only
+    //interfaces are just contracts
+    public interface IActorsService
+    {
+        //get all actors from the database
+
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id); // method to return a single actor
+
+        void Add(Actor actor); // add actor to the database - no return
+
+        Actor Update(int id, Actor newActor); //update actor
+
+        void Delete(int id); // delete actor
+
+
+
+
+
+
+
+    }
+}
